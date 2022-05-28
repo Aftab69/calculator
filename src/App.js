7ce4b588f38f97435810035d5beeac0223715656
Aftab69 <aftabhssn3@gmail.com>
2022-05-28 21:41:03 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState } from "react";
+const mathjs = require('mathjs');
 
 function App() {
   const [digit, setDigit]= useState("");
@@ -10,26 +11,26 @@ function App() {
           {digit}
         </div>
         <div className="flex_container">
-              <div className="flex_item">R</div>
-              <div className="flex_item">%</div>
-              <div className="flex_item">CE</div>
-              <div className="flex_item">÷</div>
+              <div className="flex_item" onClick={()=>setDigit("")}>R</div>
+              <div className="flex_item" onClick={()=>setDigit(digit+"%")}>%</div>
+              <div className="flex_item" onClick={()=>setDigit(digit.slice(0,digit.length-1))}>CE</div>
+              <div className="flex_item" onClick={()=>setDigit(digit+"/")}>÷</div>
               <div className="flex_item" onClick={()=>setDigit(digit+"7")}>7</div>
               <div className="flex_item" onClick={()=>setDigit(digit+"8")}>8</div>
               <div className="flex_item" onClick={()=>setDigit(digit+"9")}>9</div>
-              <div className="flex_item">x</div>
+              <div className="flex_item" onClick={()=>setDigit(digit+"*")}>x</div>
               <div className="flex_item" onClick={()=>setDigit(digit+"4")}>4</div>
               <div className="flex_item" onClick={()=>setDigit(digit+"5")}>5</div>
               <div className="flex_item" onClick={()=>setDigit(digit+"6")}>6</div>
-              <div className="flex_item">-</div>
+              <div className="flex_item" onClick={()=>setDigit(digit+"-")}>-</div>
               <div className="flex_item" onClick={()=>setDigit(digit+"1")}>1</div>
               <div className="flex_item" onClick={()=>setDigit(digit+"2")}>2</div>
               <div className="flex_item" onClick={()=>setDigit(digit+"3")}>3</div>
-              <div className="flex_item">+</div>
+              <div className="flex_item" onClick={()=>setDigit(digit+"+")}>+</div>
               <div className="flex_item" onClick={()=>setDigit(digit+"00")}>00</div>
               <div className="flex_item" onClick={()=>setDigit(digit+"0")}>0</div>
-              <div className="flex_item">.</div>
-              <div className="flex_item">=</div>
+              <div className="flex_item" onClick={()=>setDigit(digit+".")}>.</div>
+              <div className="flex_item" onClick={()=>setDigit(mathjs.evaluate(digit))}>=</div>
         </div>
       </div>
     </>
